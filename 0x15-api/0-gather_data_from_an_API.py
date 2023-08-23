d2e8703a1d58d5a20d3423213da86db65d892bd3
brivan1 <bkryan.bk@gmail.com>
2023-08-23 14:42:25 -0700

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" script displays a TODO list for a given employee with an ID """
+
+import requests as req
+import sys
+
+if __name__ == '__main__':
+
+    employeeID = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com"
+    url = baseUrl + "/" + employeeID
+
+    response = req.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = req.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+            print("Employee {} is done with tasks({}/{}):"
+                    .format(name, done, len(tasks)))
+
+            for task in done_tasks:
+                print("\t {}".format(task.get('title')))
+
