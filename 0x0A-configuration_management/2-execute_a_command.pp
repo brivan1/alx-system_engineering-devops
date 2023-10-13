b89bae5c7773b5f9c7b6e920e2436d53aa1ed43b
brivan1 <105288360+brivan1@users.noreply.github.com>
2023-10-13 11:04:48 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-#execute pkill command to kill a process named killmenow by creating a manifest using puppet
-
-exec { 'pkill':
-  command => 'pkill -9 -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
-}
